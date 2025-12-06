50454e3b6e1af36b0c8cd8967450866290f3fe7d
bsse23094 <143380602+bsse23094@users.noreply.github.com>
2025-12-06 23:22:59 +0500

    Fix router basename for GitHub Pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,7 @@ import "./styles/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/who-is-the-clown">
       <App />
     </BrowserRouter>
   </React.StrictMode>
